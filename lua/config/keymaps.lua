580923611bcb0a122428a6b4e20d20e772db6e7f
antithezis <juandbm2001@gmail.com>
2024-05-04 19:58:47 -0500

    Remove keymap to remove all buffers but current one

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,3 @@
 -- Navigate between buffers --
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Navigate to the next buffer" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Navigate to the previous buffer" })
-
--- Delete all buffers but the current one --
-vim.keymap.set(
-  "n",
-  "<leader>bq",
-  '<Esc>:%bdelete|edit #|normal`"<Return>',
-  { desc = "Delete other buffers but the current one" }
-)
